878f74378957a70d8f2818cd29e3e7cf5055eb7b
xutl <79109681@qq.com>
2016-11-14 15:09:15 +0800

    评论

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -6,9 +6,35 @@
  */
 namespace yuncms\comment\controllers;
 
+use Yii;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
+/**
+ * Class DefaultController
+ * @package yuncms\comment\controllers
+ */
 class DefaultController extends Controller
 {
-    
+    public function actionComment(){
+        $sourceType = Yii::$app->request->post('sourceType');
+        $sourceId = Yii::$app->request->post('sourceId');
+
+        $source = null;
+        if ($sourceType == 'question') {
+            $source = Question::findOne($sourceId);
+            $subject = $source->title;
+        } else if ($sourceType == 'user') {
+            $userClass = Yii::$app->user->identityClass;
+            $source = $userClass::find()->with('userData')->where(['id'=>$sourceId])->one();
+            $subject = $source->username;
+        } else if ($sourceType == 'code') {
+            $source = Code::findOne($sourceId);
+            $subject = $source->title;
+        }
+        if (!$source) {
+            throw new NotFoundHttpException ();
+        }
+
+    }
 }
