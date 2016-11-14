cffef5ad0912c4b4f2a5988d8c405f1590402710
xutl <79109681@qq.com>
2016-11-14 15:03:39 +0800

    update

---
+++ widgets/Comment.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\comment\widgets;
+
+use yii\base\Widget;
+
+class Comment extends Widget
+{
+    
+}
